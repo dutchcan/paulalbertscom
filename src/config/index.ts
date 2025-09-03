b7b1bed1382d3b551b2db393c65555beeaba57d3
dutchcan <41980349+dutchcan@users.noreply.github.com>
2025-09-03 12:57:40 -0700

    Update index.ts

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,19 +1,23 @@
 import type { SiteConfig, SiteContent } from "../types";
 
 export const SITE_CONFIG: SiteConfig = {
-  title: "Paul Alberts",
-  author: "Paul Alberts",
+  title: "Alejandro Múnez — Mobile & Web Developer",
+  author: "Alejandro Múnez Cuntez",
   description:
-    "Crafting Stories. Coordinating Emergencies. Leading with Meaning.",
+    "Software Engineer based in San Francisco, USA. I specialize in UI design, web and mobile application development and maintenance.",
   lang: "en",
-  siteLogo: "/paul-small.jpg",
+  siteLogo: "/alejandro-small.jpg",
   navLinks: [
     { text: "Experience", href: "#experience" },
+    { text: "Projects", href: "#projects" },
     { text: "About", href: "#about" },
   ],
   socialLinks: [
-    { text: "LinkedIn", href: "https://linkedin.com/paulalberts"},
-    { text: "Youtube", href: "https://youtube.com/paulalberts"},
+    { text: "Twitter", href: "https://github.com/immois/astro-zen" },
+    { text: "LinkedIn", href: "https://github.com/immois/astro-zen" },
+    { text: "Github", href: "https://github.com/immois/astro-zen" },
+    { text: "Youtube", href: "https://github.com/immois/astro-zen" },
+    { text: "Dribbble", href: "https://github.com/immois/astro-zen" },
   ],
   socialImage: "/zen-og.png",
   canonicalURL: "https://astro-zen.vercel.app",
@@ -21,51 +25,44 @@ export const SITE_CONFIG: SiteConfig = {
 
 export const SITE_CONTENT: SiteContent = {
   hero: {
-    name: "Paul Alberts",
-    specialty: "From Communications to Leadership on the Frontlines of Public Safety.",
+    name: "Alejandro Múnez",
+    specialty: "Mobile & Web Developer",
     summary:
-      "Visual storytelling. Emergency coordination. Purposeful leadership. Relentless innovation.",
-    email: "paul@paulalberts.com",
+      "Developer based in San Francisco, USA. I specialize in UI design, web and mobile application development and maintenance.",
+    email: "example@email.com",
   },
   experience: [
     {
-      company: "BC Emergency Health Services",
-      position: "Operations Manager",
-      startDate: "August 2016",
-      endDate: "Present",
+      company: "Zalmart",
+      position: "Lead Android Developer",
+      startDate: "May 2018",
+      endDate: "Sept 2020",
       summary: [
-        "Operations Manager with frontline experience as a Paramedic, Dispatcher, and Supervisor.",
-        "Leads complex dispatch operations and fosters continuous improvement in emergency services.",
-        "Known for clear communication, team leadership, and patient-focused decision-making.",
+        "Implemented advanced memory management and code optimization techniques, resulting in a reduction in application load time by 40% and a decrease in crashes by 25%. This significantly improved user experience and increased user retention by 20%.",
+        "I led a team of developers in building and integrating new features using Jetpack Android components such as LiveData and ViewModel. This enabled us to build scalable and maintainable applications, reducing the crash rate by 20% and speeding up the time to delivery of new features by 15%.",
+        "Integrated Google Pay for in-app purchases, resulting in a 35% increase in mobile transaction revenue. Additionally, implemented Firebase Analytics to gain insights into user behavior, enabling data-driven optimizations and a 30% increase in user retention.",
       ],
     },
     {
-      company: "Ardor Media",
-      position: "Sole Proprietor",
+      company: "Bankit",
+      position: "Mobile Developer",
       startDate: "Feb 2017",
-      endDate: "Present",
+      endDate: "May 2018",
       summary: [
-        "",
-      ],
-    },
-        {
-      company: "Tourism Prince George",
-      position: "Community Multimedia Coordinator",
-      startDate: "2013",
-      endDate: "2015",
-      summary: [
-        "",
+        "I designed and developed a mobile application using Flutter, allowing it to be deployed on both Android and iOS with a single codebase. This reduced development time by 50% and maintenance costs by 30%, facilitating a consistent user experience on both platforms.",
+        "I integrated biometric authentication and data encryption, significantly improving the security of user data. This implementation resulted in a 40% increase in user trust and a 25% reduction in unauthorized access attempts.",
       ],
     },
     {
-      company: "City of Prince George",
-      position: "Multimedia Coordinator",
-      startDate: "2008",
-      endDate: "2012",
+      company: "Driveer",
+      position: "Frontend Developer",
+      startDate: "Jun 2015",
+      endDate: "Oct 2016",
       summary:
-        "",
+        "Developed and integrated a real-time vehicle tracking system using WebSockets, improving accuracy and data update in the application. This functionality increased user satisfaction by 30% and reduced customer service inquiries by 25%.",
     },
-     projects: [
+  ],
+  projects: [
     {
       name: "Spotifu Music",
       summary: "A music streaming app that emulates Spotify's core features.",
@@ -73,13 +70,28 @@ export const SITE_CONTENT: SiteContent = {
       linkSource: "https://github.com/immois/astro-zen",
       image: "/spotifu.png",
     },
-  ],   
+    {
+      name: "Shopp App",
+      summary: "An e-commerce platform that replicates Shopify's key features.",
+      linkPreview: "/",
+      linkSource: "https://github.com/immois/astro-zen",
+      image: "/shopify-clon.png",
+    },
+    {
+      name: "ClonTagram",
+      summary: "A social network that replicates the features of Instagram",
+      linkPreview: "/",
+      linkSource: "https://github.com/immois/astro-zen",
+      image: "/clone-ig.png",
+    },
   ],
   about: {
-    description: `After spending nearly 30 years in Northern British Columbia—where I was born, raised, and built a fulfilling career producing media and multimedia content across sectors including municipal government, health care, post-secondary education, economic development, and tourism—I wrapped up my time in the region with a milestone role in the 2015 Prince George Canada Winter Games. The Games gave me the opportunity to showcase the community on a national stage through a range of visual media, including national television commercials, broadcast footage, and digital content. 
-    
-    Leaving the industry on a high note, I was inspired to further serve the community and follow my growing interest in public service and public safety. I shifted into Emergency Medical Services, starting on the frontlines as a Paramedic and later moving into a communications role as a Dispatcher. I’ve since transitioned into a leadership position, where I now lead a team of over 100 professionals who save lives every day—over the phone.`,
-    image: "/paulalberts.jpg",
+    description: `
+      Hi, I’m Alejandro Múnez, a passionate Mobile and Web Developer with a knack for crafting seamless digital experiences. With a strong background in both Android and iOS development, as well as front-end web technologies, I thrive in the intersection where creativity meets technology.
+
+      Over the years, I’ve honed my skills in building robust, user-friendly applications that not only meet the needs of users but also push the boundaries of what’s possible. My projects range from innovative mobile applications to responsive web designs, all with a focus on performance, security, and scalability.
+    `,
+    image: "/alejandro-big.jpg",
   },
 };
 
