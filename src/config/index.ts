d0188f48e2065df6a90146d3caf744a03b8e74c1
dutchcan <41980349+dutchcan@users.noreply.github.com>
2025-09-03 09:38:32 -0700

    Update index.ts

--- src/config/index.ts
+++ src/config/index.ts
@@ -65,7 +65,6 @@ export const SITE_CONTENT: SiteContent = {
       summary:
         "",
     },
-  projects: []    
   ],
   about: {
     description: `After spending nearly 30 years in Northern British Columbia—where I was born, raised, and built a fulfilling career producing media and multimedia content across sectors including municipal government, health care, post-secondary education, economic development, and tourism—I wrapped up my time in the region with a milestone role in the 2015 Prince George Canada Winter Games. The Games gave me the opportunity to showcase the community on a national stage through a range of visual media, including national television commercials, broadcast footage, and digital content. 
