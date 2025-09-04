b162eff016222b134c383e5fe6ecb507a7dd08be
dutchcan <41980349+dutchcan@users.noreply.github.com>
2025-09-04 07:44:13 -0700

    Update index.ts

--- src/config/index.ts
+++ src/config/index.ts
@@ -13,7 +13,7 @@ export const SITE_CONFIG: SiteConfig = {
     { text: "About", href: "#about" },
   ],
   socialLinks: [
-    { text: "LinkedIn", href: "https://linkedin.com/paulalberts },
+    { text: "LinkedIn", href: "https://linkedin.com/paulalberts" },
     { text: "Youtube", href: "https://youtube.com/paulalberts" },
   ],
   socialImage: "/zen-og.png",
