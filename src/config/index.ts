1dc3fcb7c40d0e24c8962b1e9e99b0cd140f2ee0
dutchcan <41980349+dutchcan@users.noreply.github.com>
2025-09-04 07:42:42 -0700

    Update index.ts

--- src/config/index.ts
+++ src/config/index.ts
@@ -4,20 +4,17 @@ export const SITE_CONFIG: SiteConfig = {
   title: "Paul Alberts",
   author: "Paul Alberts",
   description:
-    "Visual storytelling. Emergency coordination. Purposeful leadership. Relentless innovation.",
+    "Crafting Stories. Coordinating Emergencies. Leading with Meaning..",
   lang: "en",
-  siteLogo: "/paulalberts-small.jpg",
+  siteLogo: "/paul-small.jpg",
   navLinks: [
     { text: "Experience", href: "#experience" },
     { text: "Projects", href: "#projects" },
     { text: "About", href: "#about" },
   ],
   socialLinks: [
-    { text: "Twitter", href: "https://github.com/immois/astro-zen" },
-    { text: "LinkedIn", href: "https://github.com/immois/astro-zen" },
-    { text: "Github", href: "https://github.com/immois/astro-zen" },
-    { text: "Youtube", href: "https://github.com/immois/astro-zen" },
-    { text: "Dribbble", href: "https://github.com/immois/astro-zen" },
+    { text: "LinkedIn", href: "https://linkedin.com/paulalberts },
+    { text: "Youtube", href: "https://youtube.com/paulalberts" },
   ],
   socialImage: "/zen-og.png",
   canonicalURL: "https://astro-zen.vercel.app",
@@ -25,10 +22,10 @@ export const SITE_CONFIG: SiteConfig = {
 
 export const SITE_CONTENT: SiteContent = {
   hero: {
-    name: "Paul Alberts",
-    specialty: "From Communications to Leadership on the Frontlines of Public Safety.",
+    name: "Alejandro Múnez",
+    specialty: "Mobile & Web Developer",
     summary:
-      "Visual storytelling. Emergency coordination. Purposeful leadership. Relentless innovation."",
+      "Developer based in San Francisco, USA. I specialize in UI design, web and mobile application development and maintenance.",
     email: "example@email.com",
   },
   experience: [
@@ -89,7 +86,7 @@ export const SITE_CONTENT: SiteContent = {
     description: `After spending nearly 30 years in Northern British Columbia—where I was born, raised, and built a fulfilling career producing media and multimedia content across sectors including municipal government, health care, post-secondary education, economic development, and tourism—I wrapped up my time in the region with a milestone role in the 2015 Prince George Canada Winter Games. The Games gave me the opportunity to showcase the community on a national stage through a range of visual media, including national television commercials, broadcast footage, and digital content. 
     
     Leaving the industry on a high note, I was inspired to further serve the community and follow my growing interest in public service and public safety. I shifted into Emergency Medical Services, starting on the frontlines as a Paramedic and later moving into a communications role as a Dispatcher. I’ve since transitioned into a leadership position, where I now lead a team of over 100 professionals who save lives every day—over the phone.`,
-    image: "/paulalberts-big.jpg",
+    image: "/alejandro-big.jpg",
   },
 };
 
